41ce8e022b8c7cea76a26e8e0c7db2cb10748ddc
Zensonaton <zensonaton@gmail.com>
2024-01-13 20:11:42 +0600

    Иконка для уведомлений.

--- lib/main.dart
+++ lib/main.dart
@@ -73,6 +73,7 @@ Future main() async {
     config: const AudioServiceConfig(
       androidNotificationChannelName: "Flutter VK",
       androidNotificationChannelId: "com.zensonaton.fluttervk",
+      androidNotificationIcon: "drawable/notification_icon",
       androidNotificationOngoing: true,
       preloadArtwork: true,
     ),
