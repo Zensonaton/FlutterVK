efef5ea9b792650802ee2bf383b01e467616ed59
Zensonaton <zensonaton@gmail.com>
2025-02-23 19:30:35 +0500

    Система по оповещению а так же остановки воспроизведения музыки, если произошло много ошибок.

--- lib/services/player/subscribers/errors_handler.dart
+++ lib/services/player/subscribers/errors_handler.dart
@@ -1,11 +1,21 @@
 import "dart:async";
 
+import "package:flutter/material.dart";
+
+import "../../../main.dart";
+import "../../../provider/l18n.dart";
 import "../../logger.dart";
 import "../player.dart";
 import "../subscriber.dart";
 
 /// Класс подписчика на события [Player] для обработки событий ошибок.
 class ErrorsHandlerPlayerSubscriber extends PlayerSubscriber {
+  /// Время, через которое может сброситься счётчик ошибок.
+  static const Duration errorsResetTimerDuration = Duration(minutes: 5);
+
+  /// Максимальное количество ошибок за [errorsResetTimerDuration].
+  static const int maxErrorsCount = 5;
+
   static final AppLogger logger = getLogger("ErrorsHandlerPlayerSubscriber");
 
   ErrorsHandlerPlayerSubscriber(Player player)
@@ -14,14 +24,63 @@ class ErrorsHandlerPlayerSubscriber extends PlayerSubscriber {
   @override
   List<StreamSubscription> subscribe(Player player) {
     return [
+      player.isLoadedStream.listen(onIsLoaded),
       player.errorStream.listen(onError),
     ];
   }
 
+  /// Количество ошибок за эту сессию воспроизведения.
+  int _errorsCount = 0;
+
+  /// Последнее время возникновения ошибки.
+  DateTime? _lastErrorTime;
+
+  /// Добавляет ошибку в счётчик ошибок.
+  void _addError() {
+    if (_lastErrorTime != null &&
+        DateTime.now().difference(_lastErrorTime!) > errorsResetTimerDuration) {
+      logger.d("Resetting errors count");
+
+      _errorsCount = 0;
+    }
+
+    _errorsCount++;
+    _lastErrorTime = DateTime.now();
+  }
+
+  /// События запуска плеера.
+  void onIsLoaded(bool isLoaded) async {
+    if (!isLoaded) return;
+
+    _errorsCount = 0;
+  }
+
+  /// Обработка ошибок в плеере.
   void onError(String message) {
-    // TODO: Отобразить в интерфейсе.
-    // TODO: Остановить плеер.
+    final l18n = player.ref.read(l18nProvider);
+    final messenger = navigatorKey.currentContext != null
+        ? ScaffoldMessenger.maybeOf(navigatorKey.currentContext!)
+        : null;
 
     logger.e(message);
+
+    _addError();
+    logger.d("Errors count: $_errorsCount/$maxErrorsCount");
+
+    final snackBarContent = _errorsCount >= maxErrorsCount
+        ? l18n.player_playback_error(error: message)
+        : l18n.player_playback_error_stopped(error: message);
+
+    if (_errorsCount >= maxErrorsCount) {
+      player.stop();
+    }
+
+    messenger?.showSnackBar(
+      SnackBar(
+        content: Text(
+          snackBarContent,
+        ),
+      ),
+    );
   }
 }
