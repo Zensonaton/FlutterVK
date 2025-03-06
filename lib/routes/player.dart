d7242df9d67a3bbf6553bb799e25a753f3b51331
Zensonaton <zensonaton@gmail.com>
2025-03-06 13:20:46 +0500

    Реализация экрана, если плеер неактивен.

--- lib/routes/player.dart
+++ lib/routes/player.dart
@@ -1,32 +1,119 @@
 import "package:flutter/material.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:styled_text/tags/styled_text_tag.dart";
+import "package:styled_text/tags/styled_text_tag_action.dart";
+import "package:styled_text/widgets/styled_text.dart";
 
+import "../provider/l18n.dart";
+import "../provider/player.dart";
 import "../utils.dart";
 import "player/background.dart";
 import "player/desktop.dart";
 import "player/mobile.dart";
 
+/// Виджет, отображаемый в случае, если плеер не активен.
+class _InactivePlayerWidget extends ConsumerWidget {
+  const _InactivePlayerWidget();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final l18n = ref.watch(l18nProvider);
+
+    final theme = Theme.of(context);
+    final scheme = theme.colorScheme;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Card(
+            margin: EdgeInsets.zero,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                spacing: 12,
+                children: [
+                  RepaintBoundary(
+                    child: Image.asset(
+                      "assets/images/dog.gif",
+                    ),
+                  ),
+                  StyledText(
+                    text: l18n.fullscreen_no_audio,
+                    style: theme.textTheme.bodyLarge,
+                    textAlign: TextAlign.center,
+                    tags: {
+                      "bold": StyledTextTag(
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      "exit": StyledTextActionTag(
+                        (_, __) {
+                          Navigator.of(context).pop();
+                        },
+                        style: theme.textTheme.bodyLarge!.copyWith(
+                          color: scheme.primary,
+                        ),
+                      ),
+                    },
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      l18n.general_exit,
+                      style: theme.textTheme.bodyLarge!.copyWith(
+                        color: scheme.primary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 /// Route, отображаемый плеер на всё окно приложения.
 ///
 /// go_route: `/player`.
-class PlayerRoute extends StatelessWidget {
+class PlayerRoute extends ConsumerWidget {
+  /// Длительность перехода между состоянием работающего плеера, и неактивного.
+  static const Duration transitionDuration = Duration(milliseconds: 500);
+
   const PlayerRoute({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final player = ref.read(playerProvider);
+    ref.watch(playerIsLoadedProvider);
+
+    final isLoaded = player.isLoaded;
+
     final mobileLayout = isMobileLayout(context);
 
-    return Scaffold(
-      body: Stack(
-        children: [
-          const BackgroundImage(),
-          if (mobileLayout)
-            const MobilePlayerWidget()
-          else
-            const DesktopPlayerWidget(),
-        ],
-      ),
+    return AnimatedSwitcher(
+      duration: transitionDuration,
+      child: isLoaded
+          ? Scaffold(
+              body: Stack(
+                children: [
+                  const BackgroundImage(),
+                  if (mobileLayout)
+                    const MobilePlayerWidget()
+                  else
+                    const DesktopPlayerWidget(),
+                ],
+              ),
+            )
+          : const _InactivePlayerWidget(),
     );
   }
 }
