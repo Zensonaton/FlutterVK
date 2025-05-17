be3f15db68da9a404e0c47dd43fd621264248ffc
Zensonaton <zensonaton@gmail.com>
2025-05-17 13:45:11 +0500

    Избавление от ненужного `SlideFromBottomPageRoute`. `Material3PageRoute` теперь `@Deprecated`.

--- lib/widgets/page_route_builders.dart
+++ lib/widgets/page_route_builders.dart
@@ -12,6 +12,7 @@ import "package:flutter/material.dart";
 ///   ),
 /// );
 ///```
+@Deprecated("Use go_router.push() instead")
 class Material3PageRoute<T> extends PageRouteBuilder<T> {
   final WidgetBuilder builder;
 
@@ -42,75 +43,6 @@ class Material3PageRoute<T> extends PageRouteBuilder<T> {
               secondaryAnimation: secondaryAnimation,
               transitionType: SharedAxisTransitionType.horizontal,
               child: child,
-            );
-          },
-        );
-}
-
-/// Page Route, который делает эффект sliding'а нового Route снизу экрана.
-///
-/// Пример использования:
-/// ```dart
-/// Navigator.push(
-///   context,
-///   SlideFromBottomPageRoute(
-///     builder: (context) => MyRoute(),
-///   ),
-/// );
-///```
-class SlideFromBottomPageRoute<T> extends PageRouteBuilder<T> {
-  final WidgetBuilder builder;
-
-  SlideFromBottomPageRoute({
-    required this.builder,
-  }) : super(
-          pageBuilder: (
-            BuildContext context,
-            Animation animation,
-            Animation secondaryAnimation,
-          ) {
-            return builder(context);
-          },
-          transitionDuration: const Duration(
-            milliseconds: 300,
-          ),
-          reverseTransitionDuration: const Duration(
-            milliseconds: 300,
-          ),
-          transitionsBuilder: (
-            context,
-            animation,
-            secondaryAnimation,
-            child,
-          ) {
-            const begin = Offset(
-              0.0,
-              1.0,
-            );
-            const end = Offset.zero;
-
-            var slideTween = Tween(
-              begin: begin,
-              end: end,
-            ).chain(
-              CurveTween(
-                curve: Curves.easeInOutCubicEmphasized,
-              ),
-            );
-            var fadeTween = Tween(
-              begin: 0.0,
-              end: 1.0,
-            );
-
-            var offsetAnimation = animation.drive(slideTween);
-            var fadeAnimation = animation.drive(fadeTween);
-
-            return FadeTransition(
-              opacity: fadeAnimation,
-              child: SlideTransition(
-                position: offsetAnimation,
-                child: child,
-              ),
             );
           },
         );
